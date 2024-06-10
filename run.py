db92d41195452ca8e1b22a8af98e01ef77772ce3
Samson Kinyanjui <109862311+sammainahkinya1404@users.noreply.github.com>
2024-06-10 18:39:33 +0300

    Updated run.py

--- run.py
+++ run.py
@@ -13,3 +13,20 @@ data = pd.read_csv(data_path)
 
 model_path = 'nn_model.h5'
 model = load_model(model_path)
+# Data Preprocessing Function
+def preprocess_data(data, label_encoder):
+    categorical_columns = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Credit_History', 'Property_Area']
+    for col in categorical_columns:
+        data[col] = label_encoder.fit_transform(data[col])
+    
+    return data
+
+# Convert data to the appropriate type for TensorFlow
+def convert_to_tensor_input(data):
+    return np.asarray(data).astype(np.float32)
+
+# Data visualization
+st.title("Loan Prediction  Analysis")
+st.write("Visualizing the cleaned data used to train the model")
+
+st.dataframe(data.head())
